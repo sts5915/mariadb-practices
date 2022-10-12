58c1129593d14a6711cb17c682f6ac35f2593fd2
sts5915 <sts5915@naver.com>
2022-10-12 16:14:53 +0900

    ex01 correct

--- sql 연습문제/practice01.sql
+++ sql 연습문제/practice01.sql
@@ -1,6 +1,6 @@
 -- 문제1.
 -- 사번이 10944인 사원의 이름은(전체 이름)
-select concat( first_name,'', last_name) as '이름'
+select concat( first_name,'', last_name) as '10944'
 	from employees
     where emp_no = 10944;
 -- 정답 : RemkoZyda
